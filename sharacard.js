66e2d601963e15114079811a49dc907826826e16
kbstt <80958930+kbstt@users.noreply.github.com>
2022-09-24 10:01:37 +0800

    Update sharacard.js

--- sharacard.js
+++ sharacard.js
@@ -1,8 +1,10 @@
 if (window.location.href.includes("sharacardcustomer")){
  let startUrl = window.location.pathname.slice(1, window.location.pathname.length);
  if (window.location.search){startUrl += window.location.search;}
+ let uid = new URL(window.location.href).searchParams.get('unid')
  window.progressierAppRuntimeSettings = {
    startUrl: startUrl,
+   uid: uid,
    scope: ""
  }
 }
