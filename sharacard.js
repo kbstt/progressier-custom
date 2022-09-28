c7c15386df752a505aa13e3e4110dc9759e2044d
kbstt <80958930+kbstt@users.noreply.github.com>
2022-09-29 05:50:26 +0800

    Update sharacard.js

--- sharacard.js
+++ sharacard.js
@@ -6,36 +6,29 @@ function initProgressierScript(){
 
 function buildCustomManifest(){
  let initializing = setInterval(function(){
-
    let titleNode = document.getElementById('001');
    let imgNode = document.getElementById('002');
-      console.log(titleNode);
-   console.log(imgNode);
    if (!titleNode || !imgNode){return;}
-   console.log(titleNode);
-   console.log(imgNode);
-   if (!imgNode.querySelector('img') || !titleNode.textContent){return;}
+   let imgEl = imgNode.querySelector('img');
+   let titleContent = titleNode.textContent;
+   if (!imgEl || !titleContent){return;}
+   let imgSrc =  !imgEl.getAttribute('src');
+   if (!imgSrc){return;}
+   if (!imgSrc.includes('cloudfront.net')){return;}
    clearInterval(initializing);
    let startUrl = window.location.pathname.slice(1, window.location.pathname.length);
    if (window.location.search){startUrl += window.location.search;}
    let uid = new URL(window.location.href).searchParams.get('unid');
-   let obj  = {
+   let srcUrl = new URL(imgSrc);
+   srcUrl.search = "?w=512&h=512&fit=crop&auto=compress&dpr=1";
+   let icon512 = srcUrl.href;
+   window.progressierAppRuntimeSettings = {
      startUrl: startUrl,
      uid: uid,
-     scope: ""
-   }
-   obj.name = titleNode.textContent.trim();
-   let imgEl = imgNode.querySelector('img');
-   if (imgEl){
-     let src = imgEl.getAttribute('src') || "";
-     if (src.includes("cloudfront.net")){
-       let srcUrl = new URL(src);
-       srcUrl.search = "?w=512&h=512&fit=crop&auto=compress&dpr=1";
-       obj.icon512 = srcUrl.href;
-     }
-   }
-   window.progressierAppRuntimeSettings = obj;
-       
+     scope: "",
+     name: titleContent.trim(),
+     icon512: icon512
+   }; 
   initProgressierScript()
  }, 200);
 };
