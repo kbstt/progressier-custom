4c16b2ffe3ceb999650c1eeba3bc24ccc7736f17
kbstt <80958930+kbstt@users.noreply.github.com>
2023-01-24 08:01:46 +0800

    Create omenetwork.js

---
+++ omenetwork.js
@@ -0,0 +1,60 @@
+window.playingNow = {};
+
+function getTrackData(){
+	let track = document.getElementById('POC');
+	if (!track){return null;}
+	let trackString = track.textContent || "";
+	let trackSplit = trackString.split("**");
+	if (trackSplit.length !== 3){return null;}
+	return {title: trackSplit[0], artist: trackSplit[1], artwork: "https://"+trackSplit[2]};
+}
+
+function stopBroadcast(){
+	window.playingNow = {};
+	if (!navigator.mediaSession) {return;}
+	navigator.mediaSession.playbackState = "paused";
+}
+
+function startBroadcast(track){
+	//some browsers may not support this
+	if (!navigator.mediaSession) {return;}
+	window.playingNow = track;
+	navigator.mediaSession.metadata = new MediaMetadata({title: track.title, artist: track.artist, artwork: track.artwork});
+	navigator.mediaSession.playbackState = "playing";
+	//you also need to tie the controls of the compact player back to the controls of the <audio> element on the page
+	//haven't implemented functions for seekbackward, seekforward, and seekto. See reference: https://developer.mozilla.org/en-US/docs/Web/API/MediaSession
+	/*navigator.mediaSession.setActionHandler('play', function(){});
+	navigator.mediaSession.setActionHandler('pause', function(){});
+	navigator.mediaSession.setActionHandler('stop', function(){});
+	navigator.mediaSession.setActionHandler('previoustrack', function(){});
+	navigator.mediaSession.setActionHandler('nexttrack', function(){});
+	navigator.mediaSession.setActionHandler('seekbackward', function(){});
+	navigator.mediaSession.setActionHandler('seekforward',  function(){});
+	navigator.mediaSession.setActionHandler('seekto',  function(){});
+	*/
+}
+
+function setPlaybackPositionn(){
+	if (!navigator.mediaSession) {return;}
+	let duration = 1;
+	let position = 10;
+	//navigator.mediaSession.setPositionState({duration: duration, playbackRate: 1, position: position});
+}
+
+function broadcastTrackData = function(){
+	let track = getTrackData();
+	console.log(track);
+	let isNew = track.title !== window.playingNow.title || track.artist !== window.playingNow.artist || track.artwork !== window.playingNow.artwork;
+	if (!track){
+		stopBroadcast();
+	}
+	else if (isNew){
+		startBroadcast(track);
+		setPlaybackPosition();
+	}
+	else {
+		setPlaybackPosition();
+	}
+}
+
+setInterval(broadcastTrackData, 500);
