ba1de3d26e537857daa8aa173f03ead29b82329f
kbstt <80958930+kbstt@users.noreply.github.com>
2023-01-25 08:05:44 +0800

    Update omenetwork.js

--- omenetwork.js
+++ omenetwork.js
@@ -1,20 +1,10 @@
 window.playingNow = {};
 
 function getArtworkData(src){
-	try {
-		if (!src.includes('http')){src = "https:"+src;}
-		let url = new URL(src);
-		let width = url.searchParams.get('w');
-		let height = url.searchParams.get('h');
-		let sizes = width && height ? (width+'x'+height) : null;
-		let type = src.includes('.png') ? 'image/png' : 'image/jpeg';
-		let obj = {src: src, type: type};
-		if (sizes){obj.sizes = sizes;}
-		return [obj];
-	}
-	catch(err){
-		return null;
-	}
+	if (!src){return null;}
+	if (!src.includes('http')){src = "https:"+src;}
+	let type = src.includes('.png') ? 'image/png' : 'image/jpeg';
+	return [{src: src, type: type}];
 }
 
 function getTrackData(){
@@ -36,12 +26,13 @@ function stopBroadcast(){
 }
 
 function startBroadcast(track){
+	//some browsers may not support this
 	if (!navigator.mediaSession) {return;}
 	window.playingNow = track;
 	navigator.mediaSession.metadata = new MediaMetadata({title: track.title, artist: track.artist, artwork: track.artwork});
 	navigator.mediaSession.playbackState = "playing";
-	
-	//can't currently set action handlers because I can't find a reference to the audio element in the code	
+	//you also need to tie the controls of the compact player back to the controls of the <audio> element on the page
+	//haven't implemented functions for seekbackward, seekforward, and seekto. See reference: https://developer.mozilla.org/en-US/docs/Web/API/MediaSession
 	/*navigator.mediaSession.setActionHandler('play', function(){});
 	navigator.mediaSession.setActionHandler('pause', function(){});
 	navigator.mediaSession.setActionHandler('stop', function(){});
@@ -54,11 +45,9 @@ function startBroadcast(track){
 }
 
 function setPlaybackPosition(){
-	if (!navigator.mediaSession) {return;}	
-	
-	//can't currently set the playback position because I can't find a reference to the audio element in the code
-	//let duration = 1;
-	//let position = 10;
+	if (!navigator.mediaSession) {return;}
+	let duration = 1;
+	let position = 10;
 	//navigator.mediaSession.setPositionState({duration: duration, playbackRate: 1, position: position});
 }
 
