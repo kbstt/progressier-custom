3aeb10e15a7c810b598e1ea9838bfd715818bbd1
kbstt <80958930+kbstt@users.noreply.github.com>
2023-02-20 13:14:20 +0800

    Update rdkrm.js

--- rdkrm.js
+++ rdkrm.js
@@ -1,1 +1,23 @@
+if (!window.progressierCustomScriptInitialized){  
+  function setDynamicManifest(){
+     let path = window.location.href.replace(window.location.origin, "");
+     if (!path.startsWith("/ride-with/")){return;}
+     let removeFirstCharacter = path.slice(1, path.length);
+     window.progressierAppRuntimeSettings = {startUrl: path};
+  };
+	
+  function initProgressierScript(){
+    let initiating = setInterval(function(){
+      let body = document.querySelector('body');
+      if (!body){return;}
+      clearInterval(initiating);
+      let script = document.createElement('script');
+      script.setAttribute('src', 'https://progressier.com/client/script.js?id=ydUTPOlWFCjJxLmjmv0i');
+      body.appendChild(script);
+    }, 100); 
+  };
 
+  setDynamicManifest();
+  initProgressierScript();
+  window.progressierCustomScriptInitialized = true;
+}
