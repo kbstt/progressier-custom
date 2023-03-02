340f703d42faa0cb0a073cde6d3491949319ee6e
kbstt <80958930+kbstt@users.noreply.github.com>
2023-03-02 18:55:15 +0800

    Create yrpll.js

---
+++ yrpll.js
@@ -0,0 +1,10 @@
+function conditionallyInitializeProgressier(){  
+  let isAndroid = /Android/.test(window.navigator.userAgent);  
+  let isIOS = [ 'iPad Simulator',  'iPhone Simulator', 'iPod Simulator',  'iPad',  'iPhone', 'iPod' ].includes(navigator.platform) || (navigator.userAgent.includes("Mac") && "ontouchend" in document);  
+  let initializeScript = isAndroid || isIOS || window.location.pathname.startsWith('/mobile/');  
+  if (initializeScript){    
+    let script = document.createElement("script");    
+    script.setAttribute("src", "https://progressier.com/client/script.js?id=V7BHjQq0puVIGOf3mYkQ");    
+    document.querySelector('body').appendChild(script);  
+  }
+  conditionallyInitializeProgressier();
