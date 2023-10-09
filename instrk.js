f49dba555be979269a6595cfe7c690ca3e086508
kbstt <80958930+kbstt@users.noreply.github.com>
2023-10-09 18:22:24 -0500

    Update instrk.js

--- instrk.js
+++ instrk.js
@@ -1,9 +1,9 @@
 if (window.location.pathname.startsWith('/dashboard')){
   window.progressierAppRuntimeSettings = {
     uid: "merchant",
-    name: "Food Trucks",
+    name: "Instatruck",
     icon512: "https://firebasestorage.googleapis.com/v0/b/pwaa-8d87e.appspot.com/o/eWYX0PrgnbmJTIRgmYiH%2FFNBlhwPQgugmIVI.png?alt=media&token=776dacf0-9ea0-4ecc-9599-30547b277463",
-    shortName: "Food Trucks",
+    shortName: "Instatruck",
     scope: "dashboard/",
     startUrl: "dashboard/"
   }
