1a2c7d1470154bb272bc1bf8086dc25523abcef9
kbstt <80958930+kbstt@users.noreply.github.com>
2023-10-19 13:19:20 +0800

    Update ajwdgz.js

--- ajwdgz.js
+++ ajwdgz.js
@@ -1,3 +1,5 @@
-document.querySelectorAll('ins.adsbygoogle').forEach(function(a){
-  a.style['z-index'] = 2147483643;
-});
+setInterval(function(){}
+  document.querySelectorAll('ins.adsbygoogle').forEach(function(a){
+    a.style['z-index'] = 2147483643;
+  });
+}, 1500);
