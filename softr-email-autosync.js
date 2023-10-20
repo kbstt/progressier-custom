085e239cdbf08dcd87355eac27acdc18111d8514
kbstt <80958930+kbstt@users.noreply.github.com>
2023-10-21 07:39:14 +0800

    Create softr-email-autosync.js

---
+++ softr-email-autosync.js
@@ -0,0 +1,9 @@
+let searchingEmail = setInterval(function(){
+  if (!window.progressier || !window.progressier.add){return;}
+  if (!window["logged_in_user"]){return;}
+  if (!window["logged_in_user"]["softr_user_email"]){return;}
+  let email = window["logged_in_user"]["softr_user_email"].trim().toLowerCase();
+  window.progressier.add({email: email});
+  clearInterval(searchingEmail);
+}, 1500);
+searchingEmail();
