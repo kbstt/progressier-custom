0132f1fd9d8d44391f9d6b1bfc3179f36fd2eb5b
kbstt <80958930+kbstt@users.noreply.github.com>
2023-10-21 07:46:45 +0800

    Update softr-email-autosync.js

--- softr-email-autosync.js
+++ softr-email-autosync.js
@@ -1,7 +1,10 @@
-let searchingEmail = setInterval(function(){
+let searchingEmail = setInterval(async function(){
   if (!window.progressier || !window.progressier.add){return;}
   if (!window["logged_in_user"]){return;}
   if (!window["logged_in_user"]["softr_user_email"]){return;}
+  let installed = progressier.native && progressier.native.standalone;
+  let subscribed = await progressier.push.isSubscribed();
+  if (!installed && !subscribed){return;}
   let email = window["logged_in_user"]["softr_user_email"].trim().toLowerCase();
   window.progressier.add({email: email});
   clearInterval(searchingEmail);
