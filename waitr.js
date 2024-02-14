8ef184fb8d99ee3188ab89ec24a69fcb0706ae28
kbstt <80958930+kbstt@users.noreply.github.com>
2024-02-14 09:28:18 +0800

    Create waitr.js

---
+++ waitr.js
@@ -0,0 +1,13 @@
+if (window.location.pathname.startsWith('/dashboard')){    
+    window.progressierAppRuntimeSettings = {
+      uid: "admin",
+      icon512: "https://pwa.xyz/v0/b/pwaa-8d87e.appspot.com/o/eWYX0PrgnbmJTIRgmYiH%2FmFfWkJpgHdmsqlR.png?alt=media&token=2a76013d-b788-424d-85f9-bdfcb685ae98" ,
+      scope: "dashboard/",
+      startUrl: "dashboard/",
+      themeColor: "#000000"
+     }
+}
+
+let script = document.createElement('script');
+script.setAttribute('src', 'https://progressier.app/fAplvAV9Zw8p7KVdgmMY/script.js');
+document.querySelector('body').appendChild(script);
