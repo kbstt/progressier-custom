3def86db2d272de20e066b88637f256cf36aa455
kbstt <80958930+kbstt@users.noreply.github.com>
2024-02-15 17:39:34 +0800

    Update waitr.js

--- waitr.js
+++ waitr.js
@@ -4,7 +4,9 @@ if (window.location.pathname.startsWith('/dashboard')){
       icon512: "https://pwa.xyz/v0/b/pwaa-8d87e.appspot.com/o/eWYX0PrgnbmJTIRgmYiH%2FckpBwxZjZxEXInh.png?alt=media&token=e533479d-f330-47f2-8ef9-669d632bf2ab",
       scope: "dashboard/",
       startUrl: "dashboard/",
-      themeColor: "#2A2B35"
+      themeColor: "#2A2B35",
+      name: "wait-r pro",
+      shortName: "wait-r pro"
      }
 }
 
