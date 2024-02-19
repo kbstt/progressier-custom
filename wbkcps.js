b7146ba01cb5b9153052cecead0856d43dfb6ed2
kbstt <80958930+kbstt@users.noreply.github.com>
2024-02-19 14:15:48 +0800

    Update wbkcps.js

--- wbkcps.js
+++ wbkcps.js
@@ -1,16 +1,29 @@
-function dothis(){
+function pushstatus(){
   window.webkit.messageHandlers['push-permission-state'].postMessage('push-permission-state');
 }
 
+function pushrequest(){
+  window.webkit.messageHandlers['push-permission-request'].postMessage('push-permission-request');
+}
+
 function initializeBtn(){
-  let btn = document.createElement('button');
-  btn.innerHTML = "do this4";
-  btn.addEventListener('click', dothis);
-  document.querySelector('body').appendChild(btn);
+  let btn1 = document.createElement('button');
+  btn1.innerHTML = "current push status";
+  btn1.addEventListener('click', pushstatus);
+  document.querySelector('body').appendChild(btn1);
+
+  let btn2 = document.createElement('button');
+  btn2.innerHTML = "request permission";
+  btn2.addEventListener('click', pushrequest);
+  document.querySelector('body').appendChild(btn2);
 }
 
 window.addEventListener('push-permission-state', function(event) {
    alert(event.detail);
+});
+
+ window.addEventListener('push-permission-request', function(event) {
+    alert(event.detail);
 });
 
 initializeBtn();
