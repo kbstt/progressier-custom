92ee838955511c48fe2108db3e6a8d556a3661ae
kbstt <80958930+kbstt@users.noreply.github.com>
2024-02-19 14:03:25 +0800

    Update wbkcps.js

--- wbkcps.js
+++ wbkcps.js
@@ -1,12 +1,10 @@
 function dothis(){
-  alert(JSON.stringify(window.webkit.messageHandlers));
-  alert(window.webkit.messageHandlers.iosListener);
-  window.webkit.messageHandlers['push-token'].postMessage({param1: "stuff", param2: "1000"}); 
+  window.webkit.messageHandlers['print'].postMessage({param1: "stuff", param2: "1000"}); 
 }
 
 function initializeBtn(){
   let btn = document.createElement('button');
-  btn.innerHTML = "do this2";
+  btn.innerHTML = "do this3";
   btn.addEventListener('click', dothis);
   document.querySelector('body').appendChild(btn);
 }
