ff0cc50c0160cf66fa6074982c08aae27da1a6fc
kbstt <80958930+kbstt@users.noreply.github.com>
2024-02-21 11:23:03 +0800

    Update wbkcps.js

--- wbkcps.js
+++ wbkcps.js
@@ -1,13 +1,9 @@
 function pushstatus(){
-  window.webkit.messageHandlers['push-permission-state'].postMessage('push-permission-state');
+  window.webkit.messageHandlers['progressier-requests-push-status'].postMessage('progressier-requests-push-status');
 }
 
 function pushrequest(){
-  window.webkit.messageHandlers['push-permission-request'].postMessage('push-permission-request');
-}
-
-function pushtoken(){
-  window.webkit.messageHandlers['push-token'].postMessage('push-token');
+  window.webkit.messageHandlers['progressier-prompts-push-permission'].postMessage('progressier-prompts-push-permission');
 }
 
 function initializeBtn(){
@@ -20,31 +16,16 @@ function initializeBtn(){
   btn2.innerHTML = "request permission";
   btn2.addEventListener('click', pushrequest);
   document.querySelector('body').appendChild(btn2);
-
-  let btn3 = document.createElement('button');
-  btn3.innerHTML = "get token";
-  btn3.addEventListener('click', pushtoken);
-  document.querySelector('body').appendChild(btn3);
 }
 
-window.addEventListener('push-token', function(event) {
-   alert(event.detail);
-});
-
-window.addEventListener('push-notification', function(event) {
-  let token = event.detail.token;
+window.addEventListener('progressier-apn-device-token-received', function(event) {
+  let token = event.detail;
   alert(token);
-  if (token){
-    progressier.add({apnDeviceToken: token});
-  }
+  if (token){ progressier.add({apnDeviceToken: token}); }
 });
 
-window.addEventListener('push-permission-state', function(event) {
+window.addEventListener('progressier-push-status-received', function(event) {
    alert(event.detail);
-});
-
- window.addEventListener('push-permission-request', function(event) {
-    alert(event.detail);
 });
 
 initializeBtn();
