2a21f3a093eb7ca55cd8d0352a3596576e495459
kbstt <80958930+kbstt@users.noreply.github.com>
2024-05-08 09:32:32 +0800

    Update sync-tags-from-html.js

--- sync-tags-from-html.js
+++ sync-tags-from-html.js
@@ -1,14 +1,17 @@
 let searchingCurrentUserTags = setInterval(async function(){
   if (!window.progressier || !window.progressier.add){return;}
-  let currentUserTagContainer = document.getElementById('current-user-tags');
+  let currentUserTagContainer = document.getElementById('current-user-progressier-tags');
   if (!currentUserTagContainer){return;}
   let currentUserTags = currentUserTagContainer.textContent;
-  if (!currentUserTags){return;}
-  let tagSplit = currentUserTags.split(',');
+  if (!currentUserTags || window.currentUserTags === currentUserTags){return;}
+  window.currentUserTags = currentUserTags;
+  console.log(currentUserTags);
+  let tagSplit = window.currentUserTags.split(',');
   let finalTags = [];
   tagSplit.forEach(function(tag){
     let t = tag.trim();
     finalTags.push(t);
   });
+  console.log(finalTags);
   progressier.add({tags: finalTags});
 }, 1500);
