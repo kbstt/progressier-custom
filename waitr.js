c95bd1e4cbe109a3a4e66c433ea24950336518fc
kbstt <80958930+kbstt@users.noreply.github.com>
2024-05-23 17:37:21 +0800

    Update waitr.js

--- waitr.js
+++ waitr.js
@@ -1,12 +1,13 @@
 if (window.location.pathname.startsWith('/dashboard')){    
     window.progressierAppRuntimeSettings = {
       uid: "admin",
-      icon512: "https://pwa.xyz/v0/b/pwaa-8d87e.appspot.com/o/eWYX0PrgnbmJTIRgmYiH%2FbDgroEYBSpYNqNA.png?alt=media&token=d7f0036c-92ae-406c-b8ae-ff19abc03348",
+     // icon512: "https://pwa.xyz/v0/b/pwaa-8d87e.appspot.com/o/eWYX0PrgnbmJTIRgmYiH%2FbDgroEYBSpYNqNA.png?alt=media&token=d7f0036c-92ae-406c-b8ae-ff19abc03348",
+      icon512: "https://pwa.xyz/v0/b/pwaa-8d87e.appspot.com/o/eWYX0PrgnbmJTIRgmYiH%2FvKThJhzBozIvZzJ.png?alt=media&token=f45ab2ee-3e80-41f8-81a1-0dfb78e99bf5",
       scope: "dashboard/",
       startUrl: "dashboard/",
       themeColor: "#2A2B35",
-      name: "wait-r pro",
-      shortName: "wait-r pro"
+      name: "wizapt pro",
+      shortName: "wizapt pro"
      }
 }
 
