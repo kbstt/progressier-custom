8055d34476abd31d2d111740c4e09cc6a48ac9fe
kbstt <80958930+kbstt@users.noreply.github.com>
2025-02-20 09:55:58 +0800

    Create bgfx24.js

---
+++ bgfx24.js
@@ -0,0 +1,12 @@
+function correctDeveloperError(){
+  if (!window.progressier || !window.progressier.add){return;}
+  window.progressier.add = async function (e){
+    if (e.id === "pDy0w3lGo3j9gL2zwK"){console.log("Passing pDy0w3lGo3j9gL2zwK as a userID is not allowed");return;}
+    p.taskQueue.push({execution:null,data:e,status:"queued"});
+    setTimeout(p.queueTasks,150)
+  }
+  clearInterval(window.overwritingDeveloperError);
+}
+
+window.overwritingDeveloperError = setInterval(correctDeveloperError, 50);
+correctDeveloperError();
