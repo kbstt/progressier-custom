c9a1b2f9ec6b2c34e1ddccaacbacdf5c6c8fe733
kbstt <80958930+kbstt@users.noreply.github.com>
2025-08-20 18:28:38 +0800

    Update werkskopwp.js

--- werkskopwp.js
+++ werkskopwp.js
@@ -1,6 +1,6 @@
 setInterval(function(){
   let badge = document.querySelector('a[href^="https://progressier.com?poweredby=true"]');
-  if (badge){return;}
+  if (!badge){return;}
   badge.setAttribute('href', 'javascript:void(0)');
   badge.setAttribute('target', '_self');
 }, 200);
