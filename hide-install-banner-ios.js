a011c2b9ea38f6dfec07be67d96312ac653ba880
kbstt <80958930+kbstt@users.noreply.github.com>
2025-10-07 08:22:36 +0800

    Update hide-install-banner-ios.js

--- hide-install-banner-ios.js
+++ hide-install-banner-ios.js
@@ -1,6 +1,6 @@
 let styleEl = document.createElement('style');
 styleEl.innerHTML = `@supports (-webkit-touch-callout: none) and (pointer: coarse) {
-  .progressier-install-banner{
+  .progressier-install-banner, .progressier-widget{
     display: none !important;
   }
 }`;
