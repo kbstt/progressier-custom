2270ca75c150643ed059901f49049f216af2db04
kbstt <80958930+kbstt@users.noreply.github.com>
2025-10-28 07:09:12 +0800

    Update ptr.js

--- ptr.js
+++ ptr.js
@@ -3,6 +3,7 @@ window.enablingPullToRefresh = setInterval(function(){
   if (!window.progressier.detection){return;}
   if (!window.progressier.data){return;}
   if (!window.progressier.data.params){return;}
+  if (window.pullingToRefreshFn){return;}
   if (!window.progressier.data.params.pullToRefresh){clearInterval(window.enablingPullToRefresh);return;}
   if (!window.progressier.detection.isIOS()){clearInterval(window.enablingPullToRefresh);return;}
   if (!window.progressier.detection.isStandalone()){clearInterval(window.enablingPullToRefresh);return;}
